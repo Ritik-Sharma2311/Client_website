eef0830ee9379bf26ce286827b8c157f4f76d342
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-06-18 22:58:38 +0000

    Mobile_logo-commit

--- my_project/src/App.jsx
+++ my_project/src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Banner from './components/Banner'
+import Benefits from './components/Benefits'
 
 function App() {
   return (
     <>
     <Header/>
     <Banner/>
+    <Benefits/>
     </>
   )
 }
---
+++ my_project/src/components/Benefits.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import mobile_logo from '../assets/images/mobile_logo.jpg'
+
+function Benefits() {
+  return (
+    <div className='max-w-[1240px] p-2 my-10 mx-auto md:grid grid-cols-3 md:gap-20'>
+      <div className='col-span-2 text-center'>
+        <img src={mobile_logo} alt="" />
+      </div>
+      <div className='flex flex-col justify-center text-center md:text-start gap-3'>
+       
+        <p className='text-xl md:text-2xl font-semibold'>
+          <h1 className='text-green-500 font-bold  text-5xl md:text-8xl'>2.5x</h1>
+            Increase in sales during <br />non-peak hours
+        </p>
+        
+        <p className='text-xl md:text-2xl font-semibold'>
+          <h1 className='text-green-500 font-bold text-5xl md:text-8xl'>70%</h1>
+            Increase in returning <br />customers
+        </p>
+          
+        <p className='text-xl md:text-2xl font-semibold'>
+          <h1 className='text-green-500 font-bold text-5xl md:text-8xl'>3x</h1>
+            Increase in positive <br />Google reviews
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Benefits
